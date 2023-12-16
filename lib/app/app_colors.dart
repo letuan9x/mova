bbaa757b7edf75e9193394b1b845ec451ff52d36
TDev9x <letuan9x.dev@gmail.com>
2023-12-16 22:08:19 +0700

    add splash

--- lib/app/app_colors.dart
+++ lib/app/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const Color primary = Color(0xFF181A20);
+  static const Color linear = Color(0xFFE21221);
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:mova/assets/colors/app_colors.dart';
+import 'package:mova/app/app_colors.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: AppColor.primary,
         body: SafeArea(
-          child: Text('fuck', style: TextStyle(
-            color: Colors.red,
-          )),
+          child: Stack(
+            children: [
+              Center(
+                child: Image.asset(
+                    'assets/images/logo.png',
+                  width: 160,
+                  height: 160,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  child: const SpinKitFadingCircle(
+                    color: AppColor.linear,
+                    size: 60,
+                  ),
+                ),
+              ),
+            ],
+          )
         ),
       ),
       debugShowCheckedModeBanner: false,
